25e1a515c2a60cec3fc8e0e0929372863db534ce
FutileDiploma <ditzelcody@yahoo.com>
2025-04-16 19:09:58 -0400

    .01

--- sprinksync/settings.py
+++ sprinksync/settings.py
@@ -7,8 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'test1')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+# Allow localhost and 127.0.0.1 in development; override via ENV in production
+if DEBUG:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 # Application definition
 INSTALLED_APPS = [
